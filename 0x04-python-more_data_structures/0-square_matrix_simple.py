39aebbc9fbbc2a8cff2da89da399004588669c62
John Diginee <johndbizz@gmail.com>
2022-12-07 07:26:28 -0800

    Fixed bug in 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
