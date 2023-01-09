eef166febb4dde5fcd1511891a4cbbd294d1b666
John Diginee <Johndbizz@gmail.com>
2023-01-09 19:24:58 +0100

    Initial commit

---
+++ 0x0A-python-inheritance 7.23.43 PM/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A class for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class."""
+    pass
---
+++ 0x0A-python-inheritance 7.23.43 PM/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class."""
+    def area(self):
+        """Method to compute this area."""
+        raise Exception('area() is not implemented')
