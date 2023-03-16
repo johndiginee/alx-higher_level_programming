fe48876383be2eee35c5e2a7faec96a9b119a4ba
John Diginee <Johndbizz@gmail.com>
2023-03-16 14:33:27 +0100

    Added source code file for 14-model_city_fetch_by_state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Python file similar to model_state.py named 
+model_city.py that contains the class 
+definition of a City.
+"""
+
+from sys import argv
+from model_state import State, Base
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access DB and get the states.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    results = session.query(City, State).join(State)
+
+    for city, state in results.all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+            
+    session.commit()
+    session.close()
