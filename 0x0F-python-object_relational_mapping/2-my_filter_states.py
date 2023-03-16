6ed946806e5cdb47c1abee84e55a39629bd7bf57
John Diginee <Johndbizz@gmail.com>
2023-03-16 12:52:00 +0100

    Added source code file for 2-my_filter_states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name 
-starting with N
+Script that takes in an argument and displays 
+all values in the states table of hbtn_0e_0_usa 
+where name matches the argument.
 """
 
 import MySQLdb as db
@@ -16,9 +17,8 @@ if __name__ == "__main__":
     db_cursor = db_connect.cursor()
 
     db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-            ORDER BY states.id ASC")
-
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+            states.id ASC".format(argv[4]))
     rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
