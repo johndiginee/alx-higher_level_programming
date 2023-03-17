b8b205bfa165b260d0a709a917f858ab274f65eb
John Diginee <johndbizz@gmail.com>
2023-03-17 04:34:07 -0700

    Fix bug in 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that adds the State object 
+Script that adds the State object
 “Louisiana” to the database hbtn_0e_6_usa.
 """
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     """
 
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+              argv[1], argv[2], argv[3])
 
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
