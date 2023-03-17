b267a983e1f4df13d173dffad9dd646487381866
John Diginee <johndbizz@gmail.com>
2023-03-17 02:38:13 -0700

    Fix bug in 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays 
-all values in the states table of hbtn_0e_0_usa 
+Script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
 
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Access DB and get the states.
     """
     db_connect = db.connect(host="localhost", port=3306, user=argv[1],
-                         password=argv[2], db=argv[3], charset="utf8")
+                            password=argv[2], db=argv[3], charset="utf8")
     db_cursor = db_connect.cursor()
 
     db_cursor.execute(
