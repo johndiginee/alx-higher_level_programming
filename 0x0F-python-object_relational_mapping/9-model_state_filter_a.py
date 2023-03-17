f86397896790c3016caa713dd1705e56f1713d33
John Diginee <johndbizz@gmail.com>
2023-03-17 04:27:43 -0700

    Fix bug in 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects 
-that contain the letter a 
+Script that lists all State objects
+that contain the letter a
 from the database hbtn_0e_6_usa.
 """
 
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     """
 
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+              argv[1], argv[2], argv[3])
 
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
@@ -26,4 +26,5 @@ if __name__ == "__main__":
     state = session.query(State).filter(State.name.contains('a'))
     if state is not None:
         for state in states:
-        print('{0}: {1}'.format(state.id, state.name))
+            print('{0}: {1}'.format(state.id, state.name))
+    session.close()
