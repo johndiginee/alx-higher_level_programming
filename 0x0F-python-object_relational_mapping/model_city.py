6407c855fe4a0bca0fa66a135272c38d50750678
John Diginee <johndbizz@gmail.com>
2023-03-17 04:43:38 -0700

    Fix bug in model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,17 +7,11 @@ to work with MySQLAlchemy ORM.
 from model_state import State, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(Base):
-    """City class
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
-    """
+    """City class"""
 
     __tablename__ = 'cities'
-
     id = Column(Integer, primary_key=True)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
