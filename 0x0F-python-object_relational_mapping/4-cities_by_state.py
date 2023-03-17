6bc255cedf3735f19a126c186adb6345a9b33204
John Diginee <johndbizz@gmail.com>
2023-03-17 02:45:45 -0700

    Fix bug in 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Write a script that lists all cities 
+Write a script that lists all cities
 from the database hbtn_0e_4_usa.
 """
 
@@ -12,14 +12,12 @@ if __name__ == "__main__":
     Access DB and get the states.
     """
     db_connect = db.connect(host="localhost", port=3306, user=argv[1],
-                         password=argv[2], db=argv[3])
-    
+                            password=argv[2], db=argv[3])
     with db_connect.cursor() as db_cursor:
         db_cursor.execute("SELECT cities.id, cities.name, states.name \
                                 FROM cities JOIN states ON cities.state_id \
                                 = states.id ORDER BY cities.id ASC")
         rows_selected = db_cursor.fetchall()
-    
     if rows_selected is not None:
         for row in rows_selected:
             print(row)
