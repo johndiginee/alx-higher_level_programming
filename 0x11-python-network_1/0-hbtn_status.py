a6ac559aa715fc14605fb2667d3982dbde31a7f7
John Diginee <johndbizz@gmail.com>
2023-03-31 04:16:17 -0700

    Added source code file for 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
+
+
+if __name__ == "__main__":
+    """Send request."""
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        bodyResponse = response.read()
+        print("Body Response:")
+        print("\t- type: {}".format(type(bodyResponse)))
+        print("\t- content: {}".format(bodyResponse))
+        print("\t- utf8 content: {}".format(bodyResponse.decode("utf-8")))
