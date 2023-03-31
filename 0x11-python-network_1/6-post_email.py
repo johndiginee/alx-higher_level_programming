60917e1623974bb42d07db96339cee3f41bd0eb9
John Diginee <johndbizz@gmail.com>
2023-03-31 04:47:55 -0700

    Added source code file for 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    urlName = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    requestData = requests.post(urlName, data=value)
+    print(requestData.text)
