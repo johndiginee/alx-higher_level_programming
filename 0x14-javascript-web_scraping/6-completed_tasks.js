510a012599d82f72834b1a38b29507440b7e16db
johndiginee <johndbizz@gmail.com>
2023-04-25 22:38:19 +0000

    Add 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const route = process.argv[2];
+const numTasks = {};
+
+request(route, (err, res, body) => {
+  if (err) throw err;
+
+  JSON.parse(body).forEach(todo => {
+    const id = todo.userId;
+    if (!numTasks[id]) numTasks[id] = 0;
+    if (todo.completed) !numTasks[id]++;
+    if (numTasks[id] === 0) delete numTasks[id];
+  });
+  console.log(numTasks);
+});
