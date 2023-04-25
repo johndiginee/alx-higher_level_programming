a0e32693d593872835a3b8fda1e9abcc6b44812e
johndiginee <johndbizz@gmail.com>
2023-04-25 19:02:57 +0000

    Add 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const  movieId= process.argv[2];
+const route = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+request(route, (err, res, body) => {
+  if (err) throw err;
+  console.log(JSON.parse(body).title);
+});
