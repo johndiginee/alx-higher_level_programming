66f51b650a241873920d56dcb2aaae91a4c1fe13
johndiginee <johndbizz@gmail.com>
2023-04-25 18:52:20 +0000

    Add 1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const fileName = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, 'utf8', (err) => {
+  if (err) throw err;
+});
